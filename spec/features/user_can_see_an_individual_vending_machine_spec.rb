d6665ab120ef27353bf91e045f88ad08ff6d7a74
John Livingston <jclivingston316@gmail.com>
2018-05-25 10:20:28 -0600

    completes user story 1

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,19 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'they see the names of all the snacks in that vending machine' do
+     owner = Owner.create(name: "Sam's Snacks")
+     machine1  = owner.machines.create(location: "Don's Mixed Drinks")
+
+     snack1 = machine1.snacks.create(name: 'Cheetos')
+     snack2 = machine1.snacks.create(name: 'Doritos')
+     snack3 = machine1.snacks.create(name: 'Cheezits')
+
+    visit machine_path(machine1)
+
+    expect(page).to have_content(snack1.name)
+    expect(page).to have_content(snack2.name)
+    expect(page).to have_content(snack3.name)
+  end
 end
